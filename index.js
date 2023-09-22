aa5e4431efb63fa692b291617359aba310e29a97
Valeriu <hirbu2391@gmail.com>
2023-09-22 12:28:57 +0300

    tema javascrip

---
+++ index.js
@@ -0,0 +1,84 @@
+const number = 4
+
+console.log(typeof number)
+
+const str = "Valeriu"
+
+console.log(typeof str)
+
+const bool = false
+
+console.log(typeof bool )
+
+const  valoare= "abcd";
+
+if (isNaN(valoare)) {
+  console.log("Valoarea este NaN.");
+} else {
+  console.log("Valoarea nu este NaN.");
+}
+
+const undef = undefined
+
+console.log(typeof undef)
+
+const faraValoare = null
+
+console.log(typeof faraValoare)
+
+const obj = {}
+
+console.log(typeof obj)
+
+const symb = Symbol
+
+console.log(typeof symb)
+
+
+const big = BigInt
+
+console.log(typeof big)
+
+
+var adunare = 4
+
+var adunare1 = 5
+
+var adunare2 = 4
+
+console.log(adunare + adunare1 + adunare2)
+
+console.log(adunare <= adunare1 <= adunare2)
+
+
+
+
+console.log(`HELLO ` + `WORLD`)
+
+
+const exemplu = { nume: "Hirbu"}
+
+
+exemplu.prenume = `Valeriu`
+exemplu.hobby = `Fodbal`
+exemplu.virsta = "32"
+exemplu.profesie = `Tehnician CAD`
+
+console.log(exemplu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
